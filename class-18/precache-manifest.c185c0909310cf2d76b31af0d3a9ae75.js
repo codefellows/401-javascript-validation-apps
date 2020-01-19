cdf8e021cf5284c697a2a2cb4ae1d670a5d096e8
johncokos <highlander.44Wgmail.com>
2020-01-19 01:52:56 +0000

    Updates

---
+++ class-18/precache-manifest.c185c0909310cf2d76b31af0d3a9ae75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e65b5627655f45ea0376860844c72e7b",
+    "url": "/401-javascript-validation-apps/class-18/index.html"
+  },
+  {
+    "revision": "1ee9594a8d761ba05697",
+    "url": "/401-javascript-validation-apps/class-18/static/css/main.0a2fdfbc.chunk.css"
+  },
+  {
+    "revision": "f0829dc0dd02fb1cc717",
+    "url": "/401-javascript-validation-apps/class-18/static/js/2.9fffab7b.chunk.js"
+  },
+  {
+    "revision": "1ee9594a8d761ba05697",
+    "url": "/401-javascript-validation-apps/class-18/static/js/main.1fb1330b.chunk.js"
+  },
+  {
+    "revision": "0aebedb1031eb86b4ed7",
+    "url": "/401-javascript-validation-apps/class-18/static/js/runtime~main.8a798a8b.js"
+  }
+]);
